bfbc002dbb3d9d1bd36667c0af3a365743e098cd
Zala Lahovnik <zala.lahovnik@gmail.com>
2024-11-14 14:49:36 +0100

    feat: create document model

---
+++ backend/models/application.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+const ApplicationSchema = new mongoose.Schema({
+    Age: String,
+    Accessibility: String,
+    EdLevel: String,
+    Employment: Number,
+    Gender: String,
+    MentalHealth: String,
+    MainBranch: String,
+    YearsCode: Number,
+    YearsCodePro: Number,
+    Country: String,
+    ComputerSkills: Number,
+    Employed: Number,
+    ExpectedSalary: Number
+})
+
+const ApplicationModel = mongoose.model('Application', ApplicationSchema)
+
+export default ApplicationModel
