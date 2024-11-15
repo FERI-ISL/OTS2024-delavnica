2f11e4f0555764bf3c84442c507a93a99d3632d8
Zala Lahovnik <zala.lahovnik@gmail.com>
2024-11-15 12:17:34 +0100

    feat: add method for getting applications

---
+++ backend/controllers/applications-controller.js
@@ -0,0 +1,17 @@
+import express from "express";
+import ApplicationModel from "../models/application.js";
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const applications = await ApplicationModel.find({}, {_id: 0, __v: 0})
+
+        return res.status(200).json({applications})
+    } catch (err) {
+        console.log('Employment error', err)
+    }
+
+})
+
+export default router
