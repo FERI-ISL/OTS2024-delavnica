3aa057dc11190df2724d6214ab072c33612a9850
Zala Lahovnik <zala.lahovnik@gmail.com>
2024-11-15 12:17:45 +0100

    feat: add route and db connection

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,28 @@
 import express from 'express'
-const app = express()
-const PORT = 5000
 import cors from 'cors'
 
 import predictController from './controllers/predict-controller.js'
+import {createConnection, initializeDatabase} from "./connection.js";
+import applicationsController from "./controllers/applications-controller.js";
+
+
+const app = express()
+const PORT = 5000
 
 app.use(express.json())
 app.use(cors())
 
 app.use('/predict', predictController)
+app.use('/applications', applicationsController)
 
 app.get('/', (req, res) => {
     res.send('Hello, World!');
 });
 
 app.listen(PORT, () => {
+    createConnection().then((value) => {
+        console.log('Database connected')})
+    initializeDatabase()
+
     console.log(`Server is running on http://localhost:${PORT}`);
 });
